8b4404f6003dabb64634c083c08c41836d4bb275
Dmytro <matsepadmy@cy-tech.fr>
2021-11-17 14:48:16 +0100

    addNewColumnToDataFrame

--- leakStudy.py
+++ leakStudy.py
@@ -3,13 +3,22 @@
 import pandas as pd
 import numpy as np
 
+"----------------------------------------------------"
 
-importedTwitchData = pd.read_csv('LeakTwitch/all_revenues/2020/01/all_revenues.csv')
+PATH_TO_THE_CSV_FILE = 'LeakTwitch/all_revenues/2020/01/all_revenues.csv'
+importedTwitchData = pd.read_csv(PATH_TO_THE_CSV_FILE)
+
+"----------------------------------------------------"
+
+def addNewColumnToDataFrame(aDataFrame, newColumnName, newColumnData):
+    aDataFrame[newColumnName] = newColumnData
+
 
 def computeStreamersMonthlyIncome(twitchData):
     allRevenueColumns = twitchData.columns.str.contains('_gross')
     return(twitchData.iloc[:, allRevenueColumns].sum(axis=1))
 
-streamersMonthlyRevenue = computeStreamersMonthlyIncome(importedTwitchData)    
-print(streamersMonthlyRevenue)
+"----------------------------------------------------"
 
+streamersMonthlyRevenue = computeStreamersMonthlyIncome(importedTwitchData)    
+addNewColumnToDataFrame(importedTwitchData, 'total_monthly_revenue', streamersMonthlyRevenue)
