f5c5f6ef4982dc1711ef618998ad35a65ae77f88
Dmytro <matsepadmy@cy-tech.fr>
2021-11-17 15:48:04 +0100

    min-max-avg-median revenue. Ignore *.csv files

--- leakStudy.py
+++ leakStudy.py
@@ -5,6 +5,7 @@ import numpy as np
 
 "----------------------------------------------------"
 
+#CSV_FILE_NAME = 'all_revenues.csv'
 PATH_TO_THE_CSV_FILE = 'LeakTwitch/all_revenues/2020/01/all_revenues.csv'
 importedTwitchData = pd.read_csv(PATH_TO_THE_CSV_FILE)
 
@@ -17,6 +18,26 @@ def addNewColumnToDataFrame(aDataFrame, newColumnName, newColumnData):
 def computeStreamersMonthlyIncome(twitchData):
     allRevenueColumns = twitchData.columns.str.contains('_gross')
     return(twitchData.iloc[:, allRevenueColumns].sum(axis=1))
+
+
+def streamerMinimumRevenue(twitchData):
+    monthlyRevenueColumn = pd.DataFrame(twitchData, columns=['total_monthly_revenue'])
+    return (monthlyRevenueColumn.min())
+
+
+def streamerMaximumRevenue(twitchData):
+    monthlyRevenueColumn = pd.DataFrame(twitchData, columns=['total_monthly_revenue'])
+    return (monthlyRevenueColumn.max())
+
+
+def streamerAverageRevenue(twitchData):
+    monthlyRevenueColumn = pd.DataFrame(twitchData, columns=['total_monthly_revenue'])
+    return (monthlyRevenueColumn.mean())
+
+
+def streamerMedianRevenue(twitchData):
+    monthlyRevenueColumn = pd.DataFrame(twitchData, columns=['total_monthly_revenue'])
+    return (monthlyRevenueColumn.median())
 
 "----------------------------------------------------"
 
